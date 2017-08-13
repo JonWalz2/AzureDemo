78fc18c7184dedfbf2c166bce46def01c09d36df
Jonathan Walz <Jonwalz@gmail.com>
2017-08-13 17:19:07 -0400

    initial commit

---
+++ runbooks/AzureAutomation.tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+"$here\$sut"
+
+Describe 'Demo Tests'{
+    It '1 should be 1'{
+        1 | should be 1
+    }
+    It '2 should be 2'{
+        2 | should be 2
+    }
+}
